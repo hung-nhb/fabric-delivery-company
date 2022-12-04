88268336857fa2ca2b818349f0881d3ef6564e66
hung-nhb <hung.nhb@gmail.com>
2022-12-04 16:09:44 +0700

    Update Create Order Page

--- mvc/models/OrderModel.php
+++ mvc/models/OrderModel.php
@@ -1,10 +1,35 @@
 <?php
 class OrderModel extends Database
 {
-    function get_orders_by_user($user_id)
+    function get_all_products()
     {
-        $sql = "SELECT * FROM make_order A JOIN `order` B ON A.OID = B.OID WHERE CID = '$user_id'";
+        $sql = "SELECT * FROM product";
         $data = $this->get_list($sql);
         return $data;
+    }
+
+    function create_order($id, $order)
+    {
+        $sql = "SELECT MAX(`OID`) FROM `Order`";
+        $data = $this->get_one($sql);
+        $oid = $data["MAX(`OID`)"] + 1;
+        $sql = "INSERT INTO `Order` VALUES ('$oid', '2022-12-31', 'Waiting', 'Wait at least 1 day to confirm order')";
+        $this->query($sql);
+        $sql = "INSERT INTO Make_order VALUES ('$oid', (SELECT SID FROM Saler ORDER BY RAND() LIMIT 1), '$id')";
+        $this->query($sql);
+    }
+
+    function get_orders_by_user($user_id)
+    {
+        $sql = "SELECT * FROM (make_order A JOIN `order` B ON A.OID = B.OID) JOIN user ON A.SID = `UID` WHERE CID = '$user_id'";
+        $data = $this->get_list($sql);
+        return $data;
+    }
+
+    function get_user($user_id)
+    {
+        $sql = "SELECT * FROM user WHERE UID = '$user_id'";
+        $data = $this->get_one($sql);
+        return $data["First and Middle Name"] . " " . $data["Name"];
     }
 }
--- mvc/views/AllOrdersView.php
+++ mvc/views/AllOrdersView.php
@@ -1,25 +1,21 @@
-<form action="/OrderController/AllOrders/">
-    Customer ID: <input type="text" name="id">
-    <input type="submit">
-</form>
 <table border="1">
-    <caption class="caption-all-orders">
-        All Order of
-        <?php if (isset($_GET["id"])) echo $_GET["id"];
-        else echo "Someone"; ?>
+    <caption>
+        <h2>All Order of
+            <?php echo $data["fullName"] ?>
+        </h2>
     </caption>
     <tr>
         <th width="100">Order ID</th>
-        <th width="100">Saler ID</th>
+        <th width="150">Saler</th>
         <th width="150">Date created</th>
-        <th width="100">Status</th>
-        <th width="400">Note</th>
+        <th width="150">Status</th>
+        <th width="500">Note</th>
     </tr>
     <?php if ($data["orderList"]) {
         foreach ($data["orderList"] as $key => $value) { ?>
             <tr>
                 <td align="center"><?php echo $value["OID"] ?></td>
-                <td align="center"><?php echo $value["SID"] ?></td>
+                <td align="center"><?php echo $value["First and Middle Name"] . " " . $value["Name"] ?></td>
                 <td align="center"><?php echo $value["Date"] ?></td>
                 <td align="center"><?php echo $value["Status"] ?></td>
                 <td><?php echo $value["Note"] ?></td>
